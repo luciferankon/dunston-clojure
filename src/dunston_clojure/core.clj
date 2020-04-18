7a34ca5869a2e73f3e2517bd95637b67cd560f4b
Ankon Chakraborty <44019225+luciferankon@users.noreply.github.com>
2020-04-18 11:19:17 +0530

    basic version of dunston with add and mov commands

---
+++ src/dunston_clojure/core.clj
@@ -0,0 +1,39 @@
+(ns dunston-clojure.core
+  (:gen-class))
+
+(defn parse-args [args]
+  (if-not (empty? args) (clojure.string/split (first args) #",")))
+
+(defn parse-line [line]
+  (let [line-details (clojure.string/split line #" ")]
+    {:line-number (first line-details) :instruction (second line-details) :args (parse-args (rest (rest line-details)))}))
+
+(def regs (atom {:A 0 :B 0 :C 0 :D 0}))
+
+(def program "10 START\n20 MOV A,2\n30 ADD A,4\n40 ADD B,3\n50 PRN A\n60 PRN B\n70 STOP")
+
+(defn coerce-to-number [val]
+  (if (number? val)
+    val
+    (Integer/parseInt val)))
+
+(defn mov [x y]
+  (swap! regs assoc (keyword x) (coerce-to-number y)))
+
+(defn add [x y]
+  (mov x (+ (@regs (keyword x)) (coerce-to-number y))))
+
+(defn prn [x]
+  (if-let [val (@regs (keyword x))]
+    (println val)
+    (println x)))
+
+(def instruction-map {:PRN prn, :ADD add, :MOV mov})
+
+(def parsed-program (map parse-line (clojure.string/split program #"\n")))
+
+(defn execute-line [func args]
+  (if-not (nil? func) (apply func args)))
+
+(defn execute-lines [lines]
+  (doseq [lines lines]  (execute-line ((keyword (:instruction lines)) instruction-map) (:args lines))))
